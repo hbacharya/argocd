635b0c9afd4520262c5f45517d419dbb8043f48f
Hardik Acharya <hbacharya@gmail.com>
2025-01-12 21:04:46 +0530

    Add Kubernetes YAML file to create Kafka cluster

---
+++ kafka/KafkaTest/KafkaTest/TestConsumer.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaTest
+{
+    public class TestConsumer
+    {
+        public ILogger<TestConsumer> Logger { get; }
+
+        public TestConsumer(ILogger<TestConsumer> logger)
+        {
+            Logger = logger;
+        }
+
+        public void Consume()
+        {
+            Logger.LogInformation("Consuming messages...");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "test-consumer-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("topic1");
+                while (true)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume();
+                        Logger.LogInformation(
+                            $"Consumed message '{consumeResult.Message.Value}' at: '{consumeResult.TopicPartitionOffset}'");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Logger.LogError($"Error occurred: {e.Error.Reason}");
+                    }
+                }
+            }
+        }
+    }
+}
